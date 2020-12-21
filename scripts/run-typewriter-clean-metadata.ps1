4562961fc846bb867bab0f294f983197d1b47f4f
Michael Mainer <mmainer@microsoft.com>
2020-12-21 14:42:04 -0800

    Add location of transform

--- scripts/run-typewriter-clean-metadata.ps1
+++ scripts/run-typewriter-clean-metadata.ps1
@@ -1,12 +1,9 @@
 # Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 
-Write-Host "Running Typewriter..."
+Write-Host "Running Typewriter to clean the metadata..."
 Push-Location -Path $env:TypewriterDirectory
 
 # Note: I think we are only inserting the v1.0 docs. Need to verify.
 & $env:TypewriterExecutable -v Info -m $env:InputMetadataFile -o $env:OutputPath -g $env:GenerationMode -t $env:Transform -d $env:DocsDirectory
-
-Write-Host "Ran typewriter with the following command:"
-Write-Host (Get-History | Select-Object -Last 1).CommandLine -ForegroundColor Green
 
 Pop-Location
